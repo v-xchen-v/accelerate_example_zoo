ce992cb18df487d5774cfce536436fb6c9f386ed
v-xchen-v <407081305@qq.com>
2023-08-18 11:41:54 +0800

    remove empty script

--- inference_time_per_token_vs_batch_size.py
+++
@@ -1,9 +0,0 @@
-"""
-To figure out the impact of batch size in the inference time.
-Expectation: Larger batch size make it possible for decrease inference time.
-"""
-
-hf_model_id="gpt2"
-# hf_model_id="gpt2-medium"
-# hf_model_id="gpt-large"
-# hf_model_id="gpt-xl"
